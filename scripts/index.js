536c4eee62336caae6b882c4df52dcc06f47aad4
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2025-11-23 19:29:59 +0800

    feat: vercel analytics

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
-import { App } from './app-class.js';
+import { inject } from "@vercel/analytics";
+inject(); // enable Vercel Analytics
+
+import { App } from "./app-class.js";
 
 const app = new App();
 app.init();
-
